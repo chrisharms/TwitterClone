c41b866e3ae3e596cff33ed35759e33e9811e5f4
Daoi <klynch0092@gmail.com>
2020-12-04 17:32:40 -0500

    Image serializer

--- TwitterClone/Site1.Master.cs
+++ TwitterClone/Site1.Master.cs
@@ -4,13 +4,16 @@ using System.Linq;
 using System.Web;
 using System.Web.UI;
 using System.Web.UI.WebControls;
+using TwitterClassLibrary.DBObjCreator;
 
 namespace TwitterClone
 {
     public partial class Site1 : System.Web.UI.MasterPage
     {
+        const string LOGO = "Logo";
         protected void Page_Load(object sender, EventArgs e)
         {
+
             if (Session["Username"] == null && Session["Guest"] == null)
             {
                 Response.Redirect("Login.aspx");
@@ -33,6 +36,7 @@ namespace TwitterClone
                 lnkMyProfile.Visible = false;
                 lblUserInfo.Visible = false;
             }
+            InitializeWebAssets();
         }
 
         protected void btnLogout_Click(object sender, EventArgs e)
@@ -49,6 +53,17 @@ namespace TwitterClone
         {
             Session.Abandon();
             Response.Redirect("Login.aspx");
+        }
+
+        private void InitializeWebAssets()
+        {
+            Exception ex = null;
+            List<(string, dynamic, Type)> filter = new List<(string, dynamic, Type)>();
+            filter.Add(DBObjCreator.CreateFilter("Key", LOGO, typeof(string)));
+            List<object[]> records = DBObjCreator.ReadDBObjsWithWhere("TP_GetAsset", ref ex, filter);
+            string serializedData = (string)records[0][2];
+            WebAssetSerializer.DeserializeImage(serializedData, imgLogo);
+            
         }
     }
 }
---
+++ TwitterClone/WebAssetSerializer.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web.UI.WebControls;
+
+namespace TwitterClone
+{
+    public class WebAssetSerializer
+    {
+        public static string SerializeImage(string path)
+        {
+            byte[] imageArray = File.ReadAllBytes(@path);
+            return Convert.ToBase64String(imageArray);
+        }
+
+        public static void DeserializeImage(string base64String, Image img)
+        {
+            img.ImageUrl = "data:image/png;base64," + base64String;
+        }
+    }
+}
